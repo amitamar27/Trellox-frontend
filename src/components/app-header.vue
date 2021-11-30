8fc4305ca9a08560a1a67f36a50029890a8b4e3c
amit <amitush27@gmail.com>
2021-11-30 17:51:55 +0200

    proccess header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,14 +1,28 @@
 <template>
     <header class="app-header">
         <nav class="nav">
+
+            <!-- router link to -> Home -->
             <router-link to="/">
             <button>
                 <img :src="require('@/assets/home.svg')" alt="">
             </button>   
             </router-link>
 
+            <!-- router link to -> Board -->
+            <router-link to="/board">
+            <button class="board-header-btn">
+                <img :src="require('@/assets/boards.svg')" alt="">
+                <p>Boards</p>
+            </button>   
+            </router-link>
 
-        
+
+      
+
+
+
+
 
         </nav>
     </header>
