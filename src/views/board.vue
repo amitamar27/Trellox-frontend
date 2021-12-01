7fe6c25b01fe6f7c093603ba476be69adc0e80f1
gilad abrevaya <gilada8892@gmail.com>
2021-12-01 20:01:01 +0200

    nav

--- src/views/board.vue
+++ src/views/board.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="board-container" >
+    <div class="side-nav" :class="{'show':showNavBar}">
+      <div class="nav-header">
+      <a @click="showNavBar=!showNavBar"><i class="el-icon-d-arrow-right"></i></a>
+      <!-- <button class="close-nav-btn" @click="closeNav">
+      <i class="el-icon-d-arrow-left"></i>
+      </button> -->
+      </div> 
+      <hr/>
+      <!-- <div class="navigation">
+        <a> <p> <i class="el-icon-s-home"></i> Home</p> </a>
+        <a> <p>  <i class="el-icon-edit-outline"></i> Board</p> </a>
+      </div> -->
+    </div>
     <!-- <h1>grandfather: board</h1> -->
     <!-- <div  v-if="board">{{board.groups}}</div> -->
     <!-- <div>{{x.length}}</div> -->
     <group-list v-if="board" :board="board"></group-list>
+
     <!-- <div class="scroller"></div> -->
   </div>
 </template>
@@ -12,7 +26,9 @@
 import groupList from "../components/group-list.vue";
 export default {
   data() {
-    return {};
+    return {
+      showNavBar:false
+    };
   },
   created() {
     this.$store.dispatch({ type:"loadBoard" });
@@ -21,6 +37,11 @@ export default {
     board() {
       return this.$store.getters.board;
     },
+  },
+  methods:{
+    closeNav(){
+      console.log('he');
+    }
   },
 
   components: {
