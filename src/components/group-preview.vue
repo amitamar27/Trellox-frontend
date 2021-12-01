5648c9ef3b748f16c01b4048aaa4f64db0306bad
amit <amitush27@gmail.com>
2021-12-01 15:49:04 +0200

    add dots menu

--- src/components/group-preview.vue
+++ src/components/group-preview.vue
@@ -1,32 +1,39 @@
 <template>
-
   <div class="group-preview">
     <!-- <h3 >grandson: group-preview</h3> -->
 
     <div class="group-preview-header">
       {{ group.title }}
 
-      <button class="group-preview-btn">
-        <img
-          :src="require('@/assets/dots-menu.svg')"
-          alt=""
-        />
+      <button
+        @click="openGroupMenu"
+        class="group-preview-btn"
+        draggable="false"
+      >
+        <img :src="require('@/assets/dots-menu.svg')" alt="" />
       </button>
+
+      <group-menu
+        @mousedown.stop=""
+        v-if="menuOpened"
+        @closeMenu="closeGroupMenu"
+        :group="group"
+        :title="'List actions'"
+      >
+      </group-menu>
     </div>
 
-    <card-list :tasks="group.tasks" :groupId="group.id" ></card-list>
+    <card-list :tasks="group.tasks" :groupId="group.id"></card-list>
 
     <!-- <div class="card-list">
           <div class="card-preview"></div>
       </div> -->
   </div>
-        
-
 </template>
 
 <script>
+import groupMenu from './menus-cmps/group-menu.vue'
 import draggable from "vuedraggable";
-
 import cardList from "./card-list.vue"
 export default {
   props: {
@@ -35,10 +42,27 @@ export default {
       required: true,
     }
   },
+  data() {
+    return {
+      menuOpened: false,
+      title: ''
+    }
+  },
+  methods: {
+    openGroupMenu() {
+      this.menuOpened = true
+      console.log('open');
+    },
+    closeGroupMenu() {
+      this.menuOpened = false
+      console.log('close');
+    }
+  },
   components: {
     cardList,
-    draggable
-},
+    draggable,
+    groupMenu
+  },
 
 }
 </script>
---
+++ src/components/menus-cmps/group-menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <section v-if="isAlive" class="group-menu" @click.stop="">
+    <header class="title">
+      <button class="close-btn-menu" @click="closeMenu">
+        <img :src="require('@/assets/cancel-icon.png')" />
+      </button>
+      <p>
+        {{ title }}
+      </p>
+    </header>
+    <group-Inner-Menu :groupId="group.id" />
+    <!-- <slot></slot> -->
+  </section>
+</template>
+
+
+<script>
+
+import groupInnerMenu from './group-inner-menu.vue'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    isClose: {
+      type: Boolean,
+      default: true
+    },
+    group: Object
+  },
+  data() {
+    return {
+      isAlive: false
+    }
+  },
+  methods: {
+    closeMenu() {
+      console.log('close');
+      this.isAlive = false
+      this.$emit('closeMenu')
+    }
+  },
+  mounted() {
+    this.isAlive = true
+  },
+  components: {
+    groupInnerMenu
+  }
+
+
+}
+</script>
